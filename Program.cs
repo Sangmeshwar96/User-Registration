f9d066478b0614a5a1f9fb7c60d69894829d6258
Sangmeshwar96 <patil.sangmeshwar4u@gmail.com>
2022-11-10 15:39:38 +0530

    First name Checking using ReGex

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UserRegistrationUsingRegex
+{
+    class Program
+    {   
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("* Welcome To User Registration Problem *");
+            Console.ReadLine();
+            Console.WriteLine("Enter First name :");
+            string fname = Console.ReadLine();
+            RegexValidation regex = new RegexValidation();
+            regex.CheckFirstName(fname);
+        }
+    }
+}
