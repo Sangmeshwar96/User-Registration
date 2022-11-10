0e1cb8b3f883fab83a31f0b2fcf509b2270522c2
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-11-10 16:02:34 +0530

    Checking Mobile Number Pattern Using ReGex

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -22,5 +22,51 @@ namespace UserRegistrationUsingRegex
                 Console.WriteLine(e.Message);
             }
         }
+
+        public void CheckLastName(string lastname)
+        {
+            string validateLastName = "^[A-Z][a-z]{3,20}$";
+            try
+            {
+                if (Regex.IsMatch(lastname, validateLastName))
+                    Console.WriteLine("Last Name saved succesfull");
+                else
+                    Console.WriteLine("Invalid name!!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void CheckEmail(string email)
+        {
+            string validateEmail = "^[a-z][a-zA-Z0-9._-]{3,20}@[a-z]{3,10}.(com)$";
+            try
+            {
+                if (Regex.IsMatch(email, validateEmail))
+                    Console.WriteLine(" email validate succesfull");
+                else
+                    Console.WriteLine("Invalid email!!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void CheckMobileNo(string mobile)
+        {
+            string validateMobile = "^[0-9]{1,4}\\s[0-9]{10,12}$";
+            try
+            {
+                if (Regex.IsMatch(mobile, validateMobile))
+                    Console.WriteLine(" mobile no validate succesfull");
+                else
+                    Console.WriteLine("Invalid mobile no.!!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
     }
 }
