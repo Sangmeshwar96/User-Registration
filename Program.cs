56449989b8be85373fe2b2b5070a6db2c598b470
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-11-10 15:56:12 +0530

    First Name Checking Using ReGex

--- Program.cs
+++ Program.cs
@@ -8,11 +8,14 @@ namespace UserRegistrationUsingRegex
         static void Main(string[] args)
         {
             Console.WriteLine("* Welcome To User Registration Problem *");
-            Console.ReadLine();
+            //Console.ReadLine();
             Console.WriteLine("Enter First name :");
             string fname = Console.ReadLine();
             RegexValidation regex = new RegexValidation();
             regex.CheckFirstName(fname);
+            Console.WriteLine("Enter Last name :");
+            string lname = Console.ReadLine();
+            regex.CheckLastName(lname);
         }
     }
 }
