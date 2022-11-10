98eb5a5e8cda3d3303752a037e5687220fed1f20
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-11-10 16:10:48 +0530

    Password Pattern Checking By ReGex

--- Program.cs
+++ Program.cs
@@ -8,11 +8,24 @@ namespace UserRegistrationUsingRegex
         static void Main(string[] args)
         {
             Console.WriteLine("* Welcome To User Registration Problem *");
-            Console.ReadLine();
+            //Console.ReadLine();
             Console.WriteLine("Enter First name :");
             string fname = Console.ReadLine();
             RegexValidation regex = new RegexValidation();
             regex.CheckFirstName(fname);
+            Console.WriteLine("Enter Last name :");
+            string lname = Console.ReadLine();
+            regex.CheckLastName(lname);
+            Console.WriteLine("Enter Email id :");
+            string email = Console.ReadLine();
+            regex.CheckEmail(email);
+            Console.WriteLine("Enter Mobile no. :");
+            string mobile = Console.ReadLine();
+            regex.CheckMobileNo(mobile);
+            Console.WriteLine("Enter Password :");
+            string password = Console.ReadLine();
+            regex.CheckPassword(password);
+
         }
     }
 }
