84171a8dd09fa8343bca7dee72d7fe5508357f5e
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-11-10 15:59:07 +0530

    E-mail Checking Using ReGex

--- Program.cs
+++ Program.cs
@@ -8,11 +8,17 @@ namespace UserRegistrationUsingRegex
         static void Main(string[] args)
         {
             Console.WriteLine("* Welcome To User Registration Problem *");
-            Console.ReadLine();
+            //Console.ReadLine();
             Console.WriteLine("Enter First name :");
             string fname = Console.ReadLine();
             RegexValidation regex = new RegexValidation();
             regex.CheckFirstName(fname);
+            Console.WriteLine("Enter Last name :");
+            string lname = Console.ReadLine();
+            regex.CheckLastName(lname);
+            Console.WriteLine("Enter Email id :");
+            string email = Console.ReadLine();
+            regex.CheckEmail(email);
         }
     }
 }
--- UserRegistration.cs
+++ UserRegistration.cs
@@ -22,5 +22,36 @@ namespace UserRegistrationUsingRegex
                 Console.WriteLine(e.Message);
             }
         }
+
+        public void CheckLastName(string lastname)
+        {
+           string validateLastName = "^[A-Z][a-z]{3,20}$";
+            try
+            {
+                if (Regex.IsMatch(lastname, validateLastName))
+                    Console.WriteLine("Last Name saved succesfull");
+                else
+                    Console.WriteLine("Invalid name!!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void CheckEmail(string email)
+        {
+            string validateEmail = "^[a-z][a-zA-Z0-9._-]{3,20}@[a-z]{3,10}.(com)$";
+            try
+            {
+                if (Regex.IsMatch(email, validateEmail))
+                    Console.WriteLine(" email validate succesfull");
+                else
+                    Console.WriteLine("Invalid email!!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
     }
 }
